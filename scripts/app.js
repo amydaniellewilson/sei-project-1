6ed8a4aa3ff461d07be0bcb31709e0bda3d18a60
AmyWilson <amydaniellewilson@gmail.com>
2019-05-31 15:30:33 +0100

    finished simple layout, ready to start logic

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,24 @@ const widthChoices = 4
 const heightChoices = 2
 const widthHints = 2
 const heightHints = 2
+const widthHints2 = 2
+const heightHints2 = 2
+const widthHints3 = 2
+const heightHints3 = 2
+const widthHints4 = 2
+const heightHints4 = 2
+const widthHints5 = 2
+const heightHints5 = 2
+const widthHints6 = 2
+const heightHints6 = 2
+const widthHints7 = 2
+const heightHints7 = 2
+const widthHints8 = 2
+const heightHints8 = 2
+const widthHints9 = 2
+const heightHints9 = 2
+const widthHints10 = 2
+const heightHints10 = 2
 const squares = []
 
 function init() {
@@ -13,6 +31,15 @@ function init() {
   const gridSolution = document.querySelector('.grid-layout.solution')
   const gridChoices = document.querySelector('.grid-layout.choices')
   const gridHints = document.querySelector('.grid-layout.hints')
+  const gridHints2 = document.querySelector('.grid-layout.hints2')
+  const gridHints3 = document.querySelector('.grid-layout.hints3')
+  const gridHints4 = document.querySelector('.grid-layout.hints4')
+  const gridHints5 = document.querySelector('.grid-layout.hints5')
+  const gridHints6 = document.querySelector('.grid-layout.hints6')
+  const gridHints7 = document.querySelector('.grid-layout.hints7')
+  const gridHints8 = document.querySelector('.grid-layout.hints8')
+  const gridHints9 = document.querySelector('.grid-layout.hints9')
+  const gridHints10 = document.querySelector('.grid-layout.hints10')
 
   // Grid squares for Main
   for (let i = 0; i < widthMain * heightMain; i++) {
@@ -48,6 +75,72 @@ function init() {
     squares.push(square)
     square.innerHTML = i
     gridHints.appendChild(square)
+  }
+
+  for (let i = 0; i < widthHints2 * heightHints2; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints2.appendChild(square)
+  }
+
+  for (let i = 0; i < widthHints3 * heightHints3; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints3.appendChild(square)
+  }
+
+  for (let i = 0; i < widthHints4 * heightHints4; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints4.appendChild(square)
+  }
+  for (let i = 0; i < widthHints5 * heightHints5; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints5.appendChild(square)
+  }
+  for (let i = 0; i < widthHints6 * heightHints6; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints6.appendChild(square)
+  }
+  for (let i = 0; i < widthHints7 * heightHints7; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints7.appendChild(square)
+  }
+  for (let i = 0; i < widthHints8 * heightHints8; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints8.appendChild(square)
+  }
+  for (let i = 0; i < widthHints9 * heightHints9; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints9.appendChild(square)
+  }
+  for (let i = 0; i < widthHints10 * heightHints10; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints10.appendChild(square)
   }
 
 
