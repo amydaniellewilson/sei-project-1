cad7d78b521611398d212bb4334932c67ee60772
AmyWilson <amydaniellewilson@gmail.com>
2019-05-31 15:16:43 +0100

    added first of hints

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,15 @@ const widthSolution = 4
 const heightSolution = 1
 const widthChoices = 4
 const heightChoices = 2
+const widthHints = 2
+const heightHints = 2
 const squares = []
 
 function init() {
   const gridMain = document.querySelector('.grid-layout.main')
   const gridSolution = document.querySelector('.grid-layout.solution')
   const gridChoices = document.querySelector('.grid-layout.choices')
-  console.log(gridMain)
-  console.log(gridSolution)
-  console.log(gridChoices)
+  const gridHints = document.querySelector('.grid-layout.hints')
 
   // Grid squares for Main
   for (let i = 0; i < widthMain * heightMain; i++) {
@@ -39,7 +39,15 @@ function init() {
     squares.push(square)
     square.innerHTML = i
     gridChoices.appendChild(square)
+  }
 
+  // Grid sqaures for Hints
+  for (let i = 0; i < widthHints * heightHints; i++) {
+    const square = document.createElement('div')
+    square.classList.add('grid-item','hints')
+    squares.push(square)
+    square.innerHTML = i
+    gridHints.appendChild(square)
   }
 
 
